0c0462cd75112ae78ff19b48bb34aa745e6865cf
William Fleming <william.fleming@cloudreach.com>
2020-02-07 10:45:23 +0000

    chore(test): adding more tests

--- http_server_test.go
+++ http_server_test.go
@@ -6,12 +6,13 @@ import (
 	"testing"
 )
 
-func TestHttpServer_Get(t *testing.T) {
+func TestClient_GetHttpServer(t *testing.T) {
 	out := `{"test":[{"createdDate":"2020-02-06 15:28:07","createdBy":"William Fleming (william.fleming@cloudreach.com)","enabled":1,"savedEvent":0,"testId":122621,"testName":"test123","type":"http-server","interval":300,"url":"https://dashboards.coqa.cloudreach.com","protocol":"TCP","ipv6Policy":"USE_AGENT_POLICY","networkMeasurements":1,"mtuMeasurements":1,"bandwidthMeasurements":0,"bgpMeasurements":1,"usePublicBgp":1,"alertsEnabled":1,"liveShare":0,"httpTimeLimit":5,"httpTargetTime":1000,"httpVersion":2,"followRedirects":1,"sslVersionId":0,"verifyCertificate":1,"useNtlm":0,"authType":"NONE","contentRegex":"","probeMode":"AUTO","agents":[{"agentId":48620,"agentName":"Seattle, WA (Trial) - IPv6","agentType":"Cloud","countryId":"US","ipAddresses":["135.84.184.153"],"location":"Seattle Area","network":"Astute Hosting Inc. (AS 54527)","prefix":"135.84.184.0/22"}],"sharedWithAccounts":[{"aid":176592,"name":"Cloudreach"}],"bgpMonitors":[{"monitorId":64,"ipAddress":"2001:240:100:ff::2497:2","countryId":"JP","monitorName":"Tokyo-3","network":"IIJ Internet Initiative Japan Inc. (AS 2497)","monitorType":"Public"}],"numPathTraces":3,"apiLinks":[{"rel":"self","href":"https://api.thousandeyes.com/v6/tests/1226221"},{"rel":"data","href":"https://api.thousandeyes.com/v6/web/http-server/1226221"},{"rel":"data","href":"https://api.thousandeyes.com/v6/net/metrics/1226221"},{"rel":"data","href":"https://api.thousandeyes.com/v6/net/path-vis/1226221"},{"rel":"data","href":"https://api.thousandeyes.com/v6/net/bgp-metrics/1226221"}],"sslVersion":"Auto"}]}`
 	setup()
 	var client = &Client{ApiEndpoint: server.URL, AuthToken: "foo"}
 	mux.HandleFunc("/tests/122621.json", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte(out))
+		assert.Equal(t, "GET", r.Method)
+		_, _ = w.Write([]byte(out))
 	})
 
 	// Define expected values from the API (based on the JSON we print out above)
@@ -45,9 +46,8 @@ func TestHttpServer_Get(t *testing.T) {
 		ProbeMode:             "AUTO",
 		Agents: []Agent{
 			{
-				AgentId:   48620,
-				AgentName: "Seattle, WA (Trial) - IPv6",
-				//AgentType:   "Cloud",
+				AgentId:     48620,
+				AgentName:   "Seattle, WA (Trial) - IPv6",
 				CountryId:   "US",
 				IpAddresses: []string{"135.84.184.153"},
 				Location:    "Seattle Area",
@@ -100,4 +100,20 @@ func TestHttpServer_Get(t *testing.T) {
 	teardown()
 	assert.Nil(t, err)
 	assert.Equal(t, &expected, res)
+}
+
+func TestUser_Delete(t *testing.T) {
+	setup()
+
+	mux.HandleFunc("/tests/http-server/1/delete.json", func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, "DELETE", r.Method)
+	})
+
+	var client = &Client{ApiEndpoint: server.URL, AuthToken: "foo"}
+	id := 1
+	err := client.DeleteHttpServer(id)
+
+	if err != nil {
+		t.Fatal(err)
+	}
 }
---
+++ page_load_test.go
@@ -0,0 +1,121 @@
+package thousandeyes
+
+import (
+	"github.com/stretchr/testify/assert"
+	"net/http"
+	"testing"
+)
+
+func TestClient_GetPageLoad(t *testing.T) {
+	out := `{"test":[{"createdDate":"2020-02-06 19:15:36","createdBy":"William Fleming (william.fleming@cloudreach.com)","enabled":1,"savedEvent":0,"testId":1226422,"testName":"test1","type":"page-load","interval":300,"httpInterval":300,"url":"https://dashboards.coqa.cloudreach.com","protocol":"TCP","networkMeasurements":1,"mtuMeasurements":1,"bandwidthMeasurements":0,"bgpMeasurements":1,"usePublicBgp":1,"alertsEnabled":1,"liveShare":0,"httpTimeLimit":5,"httpTargetTime":1000,"httpVersion":2,"pageLoadTimeLimit":10,"pageLoadTargetTime":6,"followRedirects":1,"includeHeaders":1,"sslVersionId":0,"verifyCertificate":1,"useNtlm":0,"authType":"NONE","contentRegex":"","probeMode":"AUTO","agents":[{"agentId":48620,"agentName":"Seattle, WA (Trial) - IPv6","agentType":"Cloud","countryId":"US","ipAddresses":["135.84.184.153"],"location":"Seattle Area","network":"Astute Hosting Inc. (AS 54527)","prefix":"135.84.184.0/22"}],"sharedWithAccounts":[{"aid":176592,"name":"Cloudreach"}],"bgpMonitors":[{"monitorId":62,"ipAddress":"2001:1890:111d:1::63","countryId":"US","monitorName":"New York, NY-6","network":"AT&T Services, Inc. (AS 7018)","monitorType":"Public"}],"numPathTraces":3,"apiLinks":[{"rel":"self","href":"https://api.thousandeyes.com/v6/tests/1226422"},{"rel":"data","href":"https://api.thousandeyes.com/v6/web/http-server/1226422"},{"rel":"data","href":"https://api.thousandeyes.com/v6/web/page-load/1226422"},{"rel":"data","href":"https://api.thousandeyes.com/v6/net/metrics/1226422"},{"rel":"data","href":"https://api.thousandeyes.com/v6/net/path-vis/1226422"},{"rel":"data","href":"https://api.thousandeyes.com/v6/net/bgp-metrics/1226422"}],"sslVersion":"Auto"}]}`
+	setup()
+	var client = &Client{ApiEndpoint: server.URL, AuthToken: "foo"}
+	mux.HandleFunc("/tests/1226422.json", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte(out))
+	})
+
+	// Define expected values from the API (based on the JSON we print out above)
+	expected := PageLoad{
+		CreatedDate:           "2020-02-06 19:15:36",
+		CreatedBy:             "William Fleming (william.fleming@cloudreach.com)",
+		Enabled:               1,
+		SavedEvent:            0,
+		TestId:                1226422,
+		TestName:              "test1",
+		Type:                  "page-load",
+		Interval:              300,
+		HttpInterval:          300,
+		Url:                   "https://dashboards.coqa.cloudreach.com",
+		Protocol:              "TCP",
+		NetworkMeasurements:   1,
+		MtuMeasurements:       1,
+		BandwidthMeasurements: 0,
+		BgpMeasurements:       1,
+		AlertsEnabled:         1,
+		LiveShare:             0,
+		HttpTimeLimit:         5,
+		HttpTargetTime:        1000,
+		HttpVersion:           2,
+		PageLoadTimeLimit:     10,
+		PageLoadTargetTime:    6,
+		IncludeHeaders:        1,
+		SslVersionId:          0,
+		VerifyCertificate:     1,
+		UseNtlm:               0,
+		AuthType:              "NONE",
+		ProbeMode:             "AUTO",
+		Agents: []Agent{
+			{
+				AgentId:     48620,
+				AgentName:   "Seattle, WA (Trial) - IPv6",
+				CountryId:   "US",
+				IpAddresses: []string{"135.84.184.153"},
+				Location:    "Seattle Area",
+				Network:     "Astute Hosting Inc. (AS 54527)",
+				Prefix:      "135.84.184.0/22",
+			},
+		},
+		SharedWithAccounts: []AccountGroup{
+			{
+				Aid:  176592,
+				Name: "Cloudreach",
+			},
+		},
+		BgpMonitors: []Monitor{
+			{
+				MonitorId:   62,
+				IpAddress:   "2001:1890:111d:1::63",
+				CountryId:   "US",
+				MonitorName: "New York, NY-6",
+				Network:     "AT&T Services, Inc. (AS 7018)",
+				MonitorType: "Public",
+			},
+		},
+		NumPathTraces: 3,
+		ApiLinks: []ApiLink{
+			{
+				Rel:  "self",
+				Href: "https://api.thousandeyes.com/v6/tests/1226422",
+			},
+			{
+				Rel:  "data",
+				Href: "https://api.thousandeyes.com/v6/web/http-server/1226422",
+			}, {
+				Rel:  "data",
+				Href: "https://api.thousandeyes.com/v6/web/page-load/1226422"},
+			{
+				Rel:  "data",
+				Href: "https://api.thousandeyes.com/v6/net/metrics/1226422",
+			},
+			{
+				Rel:  "data",
+				Href: "https://api.thousandeyes.com/v6/net/path-vis/1226422",
+			}, {
+				Rel:  "data",
+				Href: "https://api.thousandeyes.com/v6/net/bgp-metrics/1226422",
+			},
+		},
+		SslVersion: "Auto",
+	}
+
+	res, err := client.GetPageLoad(1226422)
+	teardown()
+	assert.Nil(t, err)
+	assert.Equal(t, &expected, res)
+}
+
+func TestClient_DeletePageLoad(t *testing.T) {
+	setup()
+	defer teardown()
+	mux.HandleFunc("/tests/page-load/1/delete.json", func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, "DELETE", r.Method)
+	})
+
+	var client = &Client{ApiEndpoint: server.URL, AuthToken: "foo"}
+	id := 1
+	err := client.DeletePageLoad(id)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+}
