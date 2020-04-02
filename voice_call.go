779aa53ba797c98eb562c8216b51c3d4212407ae
John Dyer <johndye@cisco.com>
2020-04-02 12:01:36 -0400

    feat(voice): add support for voice-call (fixes: #27)
    feat(voice): add support for voice-call (fixes: #27)

---
+++ voice_call.go
@@ -0,0 +1,112 @@
+package thousandeyes
+
+import (
+	"fmt"
+)
+
+type sipAuthData struct {
+	AuthUser     string `json:"authUser,omitempty"`
+	Password     string `json:"password,omitempty"`
+	Port         int    `json:"port,omitempty"`
+	Protocol     string `json:"protocol,omitempty"`
+	SipProxy     string `json:"sipProxy,omitempty"`
+	SipRegistrar string `json:"sipRegistrar,omitempty"`
+	User         string `json:"user,omitempty"`
+}
+
+// VoiceCall - VoiceCall trace test
+type VoiceCall struct {
+	Agents               []Agent        `json:"agents,omitempty"`
+	AlertsEnabled        int            `json:"alertsEnabled,omitempty"`
+	AlertRules           []AlertRule    `json:"alertRules,omitempty"`
+	APILinks             []ApiLink      `json:"apiLinks,omitempty"`
+	CreatedBy            string         `json:"createdBy,omitempty"`
+	CreatedDate          string         `json:"createdDate,omitempty"`
+	Description          string         `json:"description,omitempty"`
+	Enabled              int            `json:"enabled,omitempty"`
+	Groups               []GroupLabel   `json:"groups,omitempty"`
+	LiveShare            int            `json:"liveShare,omitempty"`
+	ModifiedBy           string         `json:"modifiedBy,omitempty"`
+	ModifiedDate         string         `json:"modifiedDate,omitempty"`
+	SavedEvent           int            `json:"savedEvent,omitempty"`
+	SharedWithAccounts   []AccountGroup `json:"sharedWithAccounts,omitempty"`
+	TestID               int            `json:"testId,omitempty"`
+	TestName             string         `json:"testName,omitempty"`
+	Type                 string         `json:"type,omitempty"`
+	Interval             int            `json:"interval,omitempty"`
+	BgpMeasurements      int            `json:"bgpMeasurements,omitempty"`
+	Dscp                 string         `json:"dscp,omitempty"`
+	DscpID               int            `json:"dscpId,omitempty"`
+	TargetSipCredentials sipAuthData    `json:"targetSipCredentials,omitempty"`
+	SourceSipCredentials sipAuthData    `json:"sourceSipCredentials,omitempty"`
+	Codec                string         `json:"codec,omitempty"`
+	CodecID              int            `json:"codecId,omitempty"`
+	Duration             int            `json:"duration,omitempty"`
+	JitterBuffer         int            `json:"jitterBuffer,omitempty"`
+	SIPTargetTime        int            `json:"sipTargetTime,omitempty"`
+	SIPTimeLimit         int            `json:"sipTimeLimit,omitempty"`
+	TargetAgentID        int            `json:"targetAgentId,omitempty"`
+}
+
+// AddAgent - Add agent to voice call  test
+func (t *VoiceCall) AddAgent(id int) {
+	agent := Agent{AgentId: id}
+	t.Agents = append(t.Agents, agent)
+}
+
+// GetVoiceCall  - get voice call test
+func (c *Client) GetVoiceCall(id int) (*VoiceCall, error) {
+	resp, err := c.get(fmt.Sprintf("/tests/%d", id))
+	if err != nil {
+		return &VoiceCall{}, err
+	}
+	var target map[string][]VoiceCall
+	if dErr := c.decodeJSON(resp, &target); dErr != nil {
+		return nil, fmt.Errorf("Could not decode JSON response: %v", dErr)
+	}
+	return &target["test"][0], nil
+}
+
+//CreateVoiceCall - Create voice call test
+func (c Client) CreateVoiceCall(t VoiceCall) (*VoiceCall, error) {
+	resp, err := c.post("/tests/voice-call/new", t, nil)
+	if err != nil {
+		return &t, err
+	}
+	if resp.StatusCode != 201 {
+		return &t, fmt.Errorf("failed to create voice-call test, response code %d", resp.StatusCode)
+	}
+	var target map[string][]VoiceCall
+	if dErr := c.decodeJSON(resp, &target); dErr != nil {
+		return nil, fmt.Errorf("Could not decode JSON response: %v", dErr)
+	}
+	return &target["test"][0], nil
+}
+
+//DeleteVoiceCall - delete voice call test
+func (c *Client) DeleteVoiceCall(id int) error {
+	resp, err := c.post(fmt.Sprintf("/tests/voice-call/%d/delete", id), nil, nil)
+	if err != nil {
+		return err
+	}
+	if resp.StatusCode != 204 {
+		return fmt.Errorf("failed to delete voice-call test, response code %d", resp.StatusCode)
+	}
+	return nil
+}
+
+//UpdateVoiceCall - - update voice call test
+func (c *Client) UpdateVoiceCall(id int, t VoiceCall) (*VoiceCall, error) {
+	resp, err := c.post(fmt.Sprintf("/tests/voice-call/%d/update", id), t, nil)
+	if err != nil {
+		return &t, err
+	}
+	if resp.StatusCode != 200 {
+		return &t, fmt.Errorf("failed to update test, response code %d", resp.StatusCode)
+	}
+	var target map[string][]VoiceCall
+	if dErr := c.decodeJSON(resp, &target); dErr != nil {
+		return nil, fmt.Errorf("Could not decode JSON response: %v", dErr)
+	}
+	return &target["test"][0], nil
+}
---
+++ voice_call_test.go
@@ -0,0 +1,269 @@
+package thousandeyes
+
+import (
+	"net/http"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestClient_GetVoiceCall(t *testing.T) {
+	out := `{"test":[{"createdDate":"2018-11-03 19:09:42","modifiedDate":"2019-02-06 01:09:56","createdBy":"ThousandEyes (support@thousandeyes.com)","modifiedBy":"ThousandEyes (support@thousandeyes.com)","enabled":1,"savedEvent":0,"testId":814641,"testName":"Voice Call - AWS SIP server","interval":120,"server":"18.234.180.66:5060","bgpMeasurements":1,"usePublicBgp":1,"duration":5,"codec":"G.711 @ 64 Kbps","codecId":0,"dscpId":46,"jitterBuffer":40,"sipTimeLimit":5,"alertsEnabled":0,"liveShare":0,"targetAgentId":69,"numPathTraces":3,"sourceSipCredentials":{"credentialsId":48162,"user":"1006","sipRegistrar":"18.234.180.66","sipProxy":"","authUser":"1006","port":5060,"protocol":"UDP"},"targetSipCredentials":{"credentialsId":48165,"user":"1005","sipRegistrar":"18.234.180.66","sipProxy":"","authUser":"1005","port":5060,"protocol":"UDP"},"sipTargetTime":1000,"dscp":"EF (DSCP 46)","apiLinks":[{"rel":"self","href":"https://api.thousandeyes.com/v6/tests/814641"},{"rel":"data","href":"https://api.thousandeyes.com/v6/voice/sip-server/814641"},{"rel":"data","href":"https://api.thousandeyes.com/v6/voice/rtp-stream/814641"},{"rel":"data","href":"https://api.thousandeyes.com/v6/net/bgp-metrics/814641"}]}]}`
+	setup()
+	var client = &Client{ApiEndpoint: server.URL, AuthToken: "foo"}
+	mux.HandleFunc("/tests/122621.json", func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, "GET", r.Method)
+		_, _ = w.Write([]byte(out))
+	})
+
+	// Define expected values from the API (based on the JSON we print out above)
+	expected := VoiceCall{
+
+		TestID:          814641,
+		Enabled:         1,
+		CreatedBy:       "ThousandEyes (support@thousandeyes.com)",
+		CreatedDate:     "2018-11-03 19:09:42",
+		SavedEvent:      0,
+		AlertsEnabled:   0,
+		TestName:        "Voice Call - AWS SIP server",
+		Interval:        120,
+		Duration:        5,
+		JitterBuffer:    40,
+		SIPTargetTime:   1000,
+		SIPTimeLimit:    5,
+		ModifiedDate:    "2019-02-06 01:09:56",
+		ModifiedBy:      "ThousandEyes (support@thousandeyes.com)",
+		TargetAgentID:   69,
+		Codec:           "G.711 @ 64 Kbps",
+		BgpMeasurements: 1,
+		Dscp:            "EF (DSCP 46)",
+		DscpID:          46,
+		TargetSipCredentials: sipAuthData{
+			Protocol:     "UDP",
+			AuthUser:     "1005",
+			Password:     "",
+			Port:         5060,
+			SipRegistrar: "18.234.180.66",
+			User:         "1005",
+		},
+		SourceSipCredentials: sipAuthData{
+			Protocol:     "UDP",
+			AuthUser:     "1006",
+			Password:     "",
+			Port:         5060,
+			SipRegistrar: "18.234.180.66",
+			User:         "1006",
+		},
+		APILinks: []ApiLink{
+
+			{
+				Rel:  "self",
+				Href: "https://api.thousandeyes.com/v6/tests/814641",
+			},
+			{
+				Rel:  "data",
+				Href: "https://api.thousandeyes.com/v6/voice/sip-server/814641",
+			},
+			{
+				Rel:  "data",
+				Href: "https://api.thousandeyes.com/v6/voice/rtp-stream/814641",
+			},
+			{
+				Rel:  "data",
+				Href: "https://api.thousandeyes.com/v6/net/bgp-metrics/814641",
+			},
+		},
+	}
+
+	res, err := client.GetVoiceCall(122621)
+	teardown()
+	assert.Nil(t, err)
+	assert.Equal(t, &expected, res)
+}
+
+func TestClient_GetVoiceCallJsonError(t *testing.T) {
+	out := `{"test":[{"createdDate":"2020-02-06 15:28:07",createdBy":"William Fleming (wfleming@grumpysysadm.com)","enabled":1,"savedEvent":0,"testId":122621,"testName":"test123","type":"sip-server","interval":300,"alertsEnabled":1,"liveShare":0,"agents":[{"agentId":48620,"agentName":"Seattle, WA (Trial) - IPv6","agentType":"Cloud","countryId":"US","ipAddresses":["135.84.184.153"],"location":"Seattle Area","network":"Astute Hosting Inc. (AS 54527)","prefix":"135.84.184.0/22"}],"sharedWithAccounts":[{"aid":176592,"name":"Cloudreach"}],"domain": "webex.com","dnsTransportProtocol":  "UDP"}]"apiLinks":[{"rel":"self","href":"https://api.thousandeyes.com/v6/tests/1226221"},{"rel":"data","href":"https://api.thousandeyes.com/v6/web/dns-trace/1226221"},{"rel":"data","href":"https://api.thousandeyes.com/v6/net/metrics/1226221"},{"rel":"data","href":"https://api.thousandeyes.com/v6/net/path-vis/1226221"},{"rel":"data","href":"https://api.thousandeyes.com/v6/net/bgp-metrics/1226221"}]}]}`
+	setup()
+	var client = &Client{ApiEndpoint: server.URL, AuthToken: "foo"}
+	mux.HandleFunc("/tests/122621.json", func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, "GET", r.Method)
+		_, _ = w.Write([]byte(out))
+	})
+	_, err := client.GetVoiceCall(122621)
+	assert.Error(t, err)
+	assert.EqualError(t, err, "Could not decode JSON response: invalid character 'c' looking for beginning of object key string")
+}
+
+func TestClient_CreateVoiceCall(t *testing.T) {
+	out := `{"test":[{"createdDate":"2018-11-03 19:09:42","modifiedDate":"2019-02-06 01:09:56","createdBy":"ThousandEyes (support@thousandeyes.com)","duration" : 5,"modifiedBy":"ThousandEyes (support@thousandeyes.com)","enabled":1,"savedEvent":0,"testId":814641,"testName":"Voice Call - AWS SIP server","interval":120,"server":"18.234.180.66:5060","bgpMeasurements":1,"usePublicBgp":1,"codec":"G.711 @ 64 Kbps","codecId":0,"dscpId":46,"alertsEnabled":0,"numPathTraces":3,"apiLinks":[{"rel":"self","href":"https://api.thousandeyes.com/v6/tests/814641"},{"rel":"data","href":"https://api.thousandeyes.com/v6/voice/sip-server/814641"},{"rel":"data","href":"https://api.thousandeyes.com/v6/voice/rtp-stream/814641"},{"rel":"data","href":"https://api.thousandeyes.com/v6/net/bgp-metrics/814641"}]}]}`
+	setup()
+	var client = &Client{ApiEndpoint: server.URL, AuthToken: "foo"}
+	mux.HandleFunc("/tests/voice-call/new.json", func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, "POST", r.Method)
+		w.WriteHeader(http.StatusCreated)
+		_, _ = w.Write([]byte(out))
+	})
+
+	// Define expected values from the API (based on the JSON we print out above)
+	expected := VoiceCall{
+		TestID:          814641,
+		Enabled:         1,
+		ModifiedBy:      "ThousandEyes (support@thousandeyes.com)",
+		ModifiedDate:    "2019-02-06 01:09:56",
+		CreatedBy:       "ThousandEyes (support@thousandeyes.com)",
+		CreatedDate:     "2018-11-03 19:09:42",
+		SavedEvent:      0,
+		TestName:        "Voice Call - AWS SIP server",
+		Interval:        120,
+		AlertsEnabled:   0,
+		DscpID:          46,
+		Duration:        5,
+		BgpMeasurements: 1,
+		Codec:           "G.711 @ 64 Kbps",
+		APILinks: []ApiLink{
+			{
+				Href: "https://api.thousandeyes.com/v6/tests/814641",
+				Rel:  "self",
+			},
+			{
+				Href: "https://api.thousandeyes.com/v6/voice/sip-server/814641",
+				Rel:  "data",
+			},
+			{
+				Href: "https://api.thousandeyes.com/v6/voice/rtp-stream/814641",
+				Rel:  "data",
+			},
+			{
+				Href: "https://api.thousandeyes.com/v6/net/bgp-metrics/814641",
+				Rel:  "data",
+			},
+		},
+	}
+	create := VoiceCall{
+		TestName: "test1",
+		DscpID:   46,
+		Duration: 5,
+		Interval: 120,
+		Codec:    "G.711 @ 64 Kbps",
+	}
+	res, err := client.CreateVoiceCall(create)
+	teardown()
+	assert.Nil(t, err)
+	assert.Equal(t, &expected, res)
+}
+
+func TestClient_DeleteVoiceCall(t *testing.T) {
+	setup()
+
+	mux.HandleFunc("/tests/voice-call/1/delete.json", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusNoContent)
+		assert.Equal(t, "POST", r.Method)
+	})
+
+	var client = &Client{ApiEndpoint: server.URL, AuthToken: "foo"}
+	id := 1
+	err := client.DeleteVoiceCall(id)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestClient_UpdateVoiceCall(t *testing.T) {
+	setup()
+	out := `{"test":[{"testId": 1234,"testName":"Voice Call - AWS SIP server","interval":120,"server":"18.234.180.66:5060","codec":"G.711 @ 64 Kbps","codecId":0,"jitterBuffer":40,"alertsEnabled":0}]}`
+	mux.HandleFunc("/tests/voice-call/1/update.json", func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, "POST", r.Method)
+		_, _ = w.Write([]byte(out))
+	})
+
+	var client = &Client{ApiEndpoint: server.URL, AuthToken: "foo"}
+	id := 1
+	sipS := VoiceCall{
+		TestName:     "Voice Call - AWS SIP server",
+		CodecID:      0,
+		Codec:        "G.711 @ 64 Kbps",
+		JitterBuffer: 40,
+	}
+	res, err := client.UpdateVoiceCall(id, sipS)
+	if err != nil {
+		t.Fatal(err)
+	}
+	expected := VoiceCall{Interval: 120, TestID: 1234, Codec: "G.711 @ 64 Kbps", TestName: "Voice Call - AWS SIP server", CodecID: 0, JitterBuffer: 40}
+	assert.Equal(t, &expected, res)
+
+}
+
+func TestVoiceCall_AddAgent(t *testing.T) {
+	test := VoiceCall{TestName: "test", Agents: Agents{}}
+	expected := VoiceCall{TestName: "test", Agents: []Agent{{AgentId: 1}}}
+	test.AddAgent(1)
+	assert.Equal(t, expected, test)
+}
+
+func TestClient_GetVoiceCallError(t *testing.T) {
+	setup()
+	var client = &Client{ApiEndpoint: server.URL, AuthToken: "foo"}
+	mux.HandleFunc("/tests/voice-call/1.json", func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, "GET", r.Method)
+		w.WriteHeader(http.StatusBadRequest)
+	})
+
+	_, err := client.GetVoiceCall(1)
+	teardown()
+	assert.Error(t, err)
+}
+
+func TestClient_GetVoiceCallStatusCode(t *testing.T) {
+	setup()
+	out := `{"test":[{"testId":1,"testName":"test123","type":"sip-server"}]}`
+	var client = &Client{ApiEndpoint: server.URL, AuthToken: "foo"}
+	mux.HandleFunc("/tests/1.json", func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, "GET", r.Method)
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(out))
+	})
+
+	_, err := client.GetVoiceCall(1)
+	teardown()
+	assert.EqualError(t, err, "Failed call API endpoint. HTTP response code: 400. Error: &{}")
+}
+
+func TestClient_CreateVoiceCallStatusCode(t *testing.T) {
+	setup()
+	var client = &Client{ApiEndpoint: server.URL, AuthToken: "foo"}
+	mux.HandleFunc("/tests/voice-call/new.json", func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, "POST", r.Method)
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(`{}`))
+	})
+	_, err := client.CreateVoiceCall(VoiceCall{})
+	teardown()
+	assert.EqualError(t, err, "Failed call API endpoint. HTTP response code: 400. Error: &{}")
+}
+
+func TestClient_UpdateVoiceCallStatusCode(t *testing.T) {
+	setup()
+	var client = &Client{ApiEndpoint: server.URL, AuthToken: "foo"}
+	mux.HandleFunc("/tests/voice-call/1/update.json", func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, "POST", r.Method)
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(`{}`))
+	})
+	_, err := client.UpdateVoiceCall(1, VoiceCall{})
+	teardown()
+	assert.EqualError(t, err, "Failed call API endpoint. HTTP response code: 400. Error: &{}")
+}
+
+func TestClient_DeleteVoiceCallStatusCode(t *testing.T) {
+	setup()
+	var client = &Client{ApiEndpoint: server.URL, AuthToken: "foo"}
+	mux.HandleFunc("/tests/voice-call/1/delete.json", func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, "POST", r.Method)
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(`{}`))
+	})
+	err := client.DeleteVoiceCall(1)
+	teardown()
+	assert.EqualError(t, err, "Failed call API endpoint. HTTP response code: 400. Error: &{}")
+}
