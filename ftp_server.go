6f823ed7c4f5fb3b110ff67ec26d3cbaf4280d5f
William Fleming <wfleming@grumpysysadm.com>
2020-04-04 16:22:05 +0100

    feat(ftp-server): add support for ftp server test

--- ftp_server.go
+++ ftp_server.go
@@ -1,15 +1,8 @@
 package thousandeyes
 
-import (
-	"fmt"
-)
+import "fmt"
 
-type Server struct {
-	ServerID   int    `json:"serverId,omitempty"`
-	ServerName string `json:"serverName,omitempty"`
-}
-
-type DNSServer struct {
+type FTPServer struct {
 	Agents                []Agent        `json:"agents,omitempty"`
 	AlertsEnabled         int            `json:"alertsEnabled,omitempty"`
 	AlertRules            []AlertRule    `json:"alertRules,omitempty"`
@@ -29,74 +22,79 @@ type DNSServer struct {
 	Type                  string         `json:"type,omitempty"`
 	BandwidthMeasurements int            `json:"bandwidthMeasurements,omitempty"`
 	BgpMeasurements       int            `json:"bgpMeasurements,omitempty"`
-	BgpMonitors           []Monitor      `json:"bgpMonitors,omitempty"`
-	DNSServers            []Server       `json:"dnsServers,omitempty"`
-	DNSTransportProtocol  string         `json:"dnsTransportProtocol,omitempty"`
 	Domain                string         `json:"domain,omitempty"`
-	Interval              int            `json:"interval,omitempty"`
 	MtuMeasurements       int            `json:"mtuMeasurements,omitempty"`
 	NetworkMeasurements   int            `json:"networkMeasurements,omitempty"`
 	NumPathTraces         int            `json:"numPathTraces,omitempty"`
-	ProbeMode             string         `json:"probeMode,omitempty"`
 	Protocol              string         `json:"protocol,omitempty"`
-	RecursiveQueries      int            `json:"recursiveQueries,omitempty"`
+	DownloadLimit         int            `json:"downloadLimit,omitempty"`
+	FtpTargetTime         int            `json:"ftpTargetTime,omitempty"`
+	FtpTimeLimit          int            `json:"ftpTimeLimit,omitempty"`
+	Password              string         `json:"password,omitempty"`
+	PathTraceMode         string         `json:"pathTraceMode,omitempty"`
+	ProbeMode             string         `json:"probeMode,omitempty"`
+	RequestType           string         `json:"requestType,omitempty"`
+	Url                   string         `json:"url,omitempty"`
+	UseActiveFtp          int            `json:"useActiveFtp,omitempty"`
+	UseExplicitFtps       int            `json:"useExplicitFtps,omitempty"`
+	Username              string         `json:"username,omitempty"`
 }
 
-// AddAgent - Add dns server test
-func (t *DNSServer) AddAgent(id int) {
+// AddAgent - Add ftp server test
+func (t *FTPServer) AddAgent(id int) {
 	agent := Agent{AgentId: id}
 	t.Agents = append(t.Agents, agent)
 }
 
 // AddAlertRule - Adds an alert to agent test
-func (t *DNSServer) AddAlertRule(id int) {
+func (t *FTPServer) AddAlertRule(id int) {
 	alertRule := AlertRule{RuleId: id}
 	t.AlertRules = append(t.AlertRules, alertRule)
 }
 
-// GetDNSServer - get dns server test
-func (c *Client) GetDNSServer(id int) (*DNSServer, error) {
+// GetFTPServer - get ftp server test
+func (c *Client) GetFTPServer(id int) (*FTPServer, error) {
 	resp, err := c.get(fmt.Sprintf("/tests/%d", id))
 	if err != nil {
-		return &DNSServer{}, err
+		return &FTPServer{}, err
 	}
-	var target map[string][]DNSServer
+	var target map[string][]FTPServer
 	if dErr := c.decodeJSON(resp, &target); dErr != nil {
 		return nil, fmt.Errorf("Could not decode JSON response: %v", dErr)
 	}
 	return &target["test"][0], nil
 }
 
-// CreateDNSServer - Create dns server test
-func (c Client) CreateDNSServer(t DNSServer) (*DNSServer, error) {
-	resp, err := c.post("/tests/dns-server/new", t, nil)
+// CreateFTPServer - Create ftp server test
+func (c Client) CreateFTPServer(t FTPServer) (*FTPServer, error) {
+	resp, err := c.post("/tests/ftp-server/new", t, nil)
 	if err != nil {
 		return &t, err
 	}
 	if resp.StatusCode != 201 {
 		return &t, fmt.Errorf("failed to create test, response code %d", resp.StatusCode)
 	}
-	var target map[string][]DNSServer
+	var target map[string][]FTPServer
 	if dErr := c.decodeJSON(resp, &target); dErr != nil {
 		return nil, fmt.Errorf("Could not decode JSON response: %v", dErr)
 	}
 	return &target["test"][0], nil
 }
 
-//DeleteDNSServer - delete dns server test
-func (c *Client) DeleteDNSServer(id int) error {
-	resp, err := c.post(fmt.Sprintf("/tests/dns-server/%d/delete", id), nil, nil)
+//DeleteFTPServer - delete ftp server test
+func (c *Client) DeleteFTPServer(id int) error {
+	resp, err := c.post(fmt.Sprintf("/tests/ftp-server/%d/delete", id), nil, nil)
 	if err != nil {
 		return err
 	}
 	if resp.StatusCode != 204 {
-		return fmt.Errorf("failed to delete dns server test, response code %d", resp.StatusCode)
+		return fmt.Errorf("failed to delete ftp server test, response code %d", resp.StatusCode)
 	}
 	return nil
 }
 
-//UpdateDNSServer - - Update dns server test
-func (c *Client) UpdateDNSServer(id int, t DNSServer) (*DNSServer, error) {
+//UpdateFTPServer - - Update ftp server test
+func (c *Client) UpdateFTPServer(id int, t FTPServer) (*FTPServer, error) {
 	resp, err := c.post(fmt.Sprintf("/tests/dns-server/%d/update", id), t, nil)
 	if err != nil {
 		return &t, err
@@ -104,7 +102,7 @@ func (c *Client) UpdateDNSServer(id int, t DNSServer) (*DNSServer, error) {
 	if resp.StatusCode != 200 {
 		return &t, fmt.Errorf("failed to update test, response code %d", resp.StatusCode)
 	}
-	var target map[string][]DNSServer
+	var target map[string][]FTPServer
 	if dErr := c.decodeJSON(resp, &target); dErr != nil {
 		return nil, fmt.Errorf("Could not decode JSON response: %v", dErr)
 	}
