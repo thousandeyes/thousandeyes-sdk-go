7a00f9a0e8dc68808d56cbb3c353bd50732d3361
William Fleming <wfleming@grumpysysadm.com>
2020-04-10 12:32:06 +0100

    feat(dnssec): add support for dnssec tests

---
+++ dns_dnssec_test.go
@@ -0,0 +1,280 @@
+package thousandeyes
+
+import (
+	"net/http"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestClient_GetDNSSec(t *testing.T) {
+	out := `{"test":[{"createdDate":"2020-02-06 15:28:07","createdBy":"William Fleming (wfleming@grumpysysadm.com)","enabled":1,"savedEvent":0,"testId":122621,"testName":"test123","type":"dns-dnssec","interval":300,"alertsEnabled":1,"liveShare":0,"agents":[{"agentId":48620,"agentName":"Seattle, WA (Trial) - IPv6","agentType":"Cloud","countryId":"US","ipAddresses":["135.84.184.153"],"location":"Seattle Area","network":"Astute Hosting Inc. (AS 54527)","prefix":"135.84.184.0/22"}],"sharedWithAccounts":[{"aid":176592,"name":"Cloudreach"}],"domain": "webex.com","dnsTransportProtocol":  "UDP","apiLinks":[{"rel":"self","href":"https://api.thousandeyes.com/v6/tests/1226221"},{"rel":"data","href":"https://api.thousandeyes.com/v6/web/dns-dnssec/1226221"},{"rel":"data","href":"https://api.thousandeyes.com/v6/net/metrics/1226221"},{"rel":"data","href":"https://api.thousandeyes.com/v6/net/path-vis/1226221"},{"rel":"data","href":"https://api.thousandeyes.com/v6/net/bgp-metrics/1226221"}]}]}`
+	setup()
+	var client = &Client{APIEndpoint: server.URL, AuthToken: "foo"}
+	mux.HandleFunc("/tests/122621.json", func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, "GET", r.Method)
+		_, _ = w.Write([]byte(out))
+	})
+
+	expected := DNSSec{
+		TestID:        122621,
+		Enabled:       1,
+		CreatedBy:     "William Fleming (wfleming@grumpysysadm.com)",
+		CreatedDate:   "2020-02-06 15:28:07",
+		SavedEvent:    0,
+		AlertsEnabled: 1,
+		TestName:      "test123",
+		Type:          "dns-dnssec",
+		Interval:      300,
+		Domain:        "webex.com",
+		Agents: []Agent{
+			{
+				AgentID:     48620,
+				AgentType:   "Cloud",
+				AgentName:   "Seattle, WA (Trial) - IPv6",
+				CountryID:   "US",
+				IPAddresses: []string{"135.84.184.153"},
+				Location:    "Seattle Area",
+				Network:     "Astute Hosting Inc. (AS 54527)",
+				Prefix:      "135.84.184.0/22",
+			},
+		},
+		SharedWithAccounts: []AccountGroup{
+			{
+				Aid:  176592,
+				Name: "Cloudreach",
+			},
+		},
+		APILinks: []APILink{
+			{
+				Href: "https://api.thousandeyes.com/v6/tests/1226221",
+				Rel:  "self",
+			},
+			{
+				Href: "https://api.thousandeyes.com/v6/web/dns-dnssec/1226221",
+				Rel:  "data",
+			},
+			{
+				Href: "https://api.thousandeyes.com/v6/net/metrics/1226221",
+				Rel:  "data",
+			},
+			{
+				Href: "https://api.thousandeyes.com/v6/net/path-vis/1226221",
+				Rel:  "data",
+			},
+			{
+				Href: "https://api.thousandeyes.com/v6/net/bgp-metrics/1226221",
+				Rel:  "data",
+			},
+		},
+	}
+
+	res, err := client.GetDNSSec(122621)
+	teardown()
+	assert.Nil(t, err)
+	assert.Equal(t, &expected, res)
+}
+
+func TestClient_GetDNSSecJsonError(t *testing.T) {
+	out := `{"test":[{"createdDate":"2020-02-06 15:28:07",createdBy":"William Fleming (wfleming@grumpysysadm.com)","enabled":1,"savedEvent":0,"testId":122621,"testName":"test123","type":"dns-dnssec","interval":300,"alertsEnabled":1,"liveShare":0,"probeMode":"AUTO","agents":[{"agentId":48620,"agentName":"Seattle, WA (Trial) - IPv6","agentType":"Cloud","countryId":"US","ipAddresses":["135.84.184.153"],"location":"Seattle Area","network":"Astute Hosting Inc. (AS 54527)","prefix":"135.84.184.0/22"}],"sharedWithAccounts":[{"aid":176592,"name":"Cloudreach"}],"domain": "webex.com","dnsTransportProtocol":  "UDP"}]"apiLinks":[{"rel":"self","href":"https://api.thousandeyes.com/v6/tests/1226221"},{"rel":"data","href":"https://api.thousandeyes.com/v6/web/dns-dnssec/1226221"},{"rel":"data","href":"https://api.thousandeyes.com/v6/net/metrics/1226221"},{"rel":"data","href":"https://api.thousandeyes.com/v6/net/path-vis/1226221"},{"rel":"data","href":"https://api.thousandeyes.com/v6/net/bgp-metrics/1226221"}]}]}`
+	setup()
+	var client = &Client{APIEndpoint: server.URL, AuthToken: "foo"}
+	mux.HandleFunc("/tests/122621.json", func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, "GET", r.Method)
+		_, _ = w.Write([]byte(out))
+	})
+	_, err := client.GetDNSSec(122621)
+	assert.Error(t, err)
+	assert.EqualError(t, err, "Could not decode JSON response: invalid character 'c' looking for beginning of object key string")
+}
+
+func TestClient_CreateDNSSec(t *testing.T) {
+	out := `{"test":[{"createdDate":"2020-02-06 15:28:07","createdBy":"William Fleming (wfleming@grumpysysadm.com)","enabled":1,"savedEvent":0,"testId":122621,"testName":"test123","type":"dns-dnssec","interval":300,"alertsEnabled":1,"liveShare":0,"agents":[{"agentId":48620,"agentName":"Seattle, WA (Trial) - IPv6","agentType":"Cloud","countryId":"US","ipAddresses":["135.84.184.153"],"location":"Seattle Area","network":"Astute Hosting Inc. (AS 54527)","prefix":"135.84.184.0/22"}],"sharedWithAccounts":[{"aid":176592,"name":"Cloudreach"}],"domain": "webex.com","apiLinks":[{"rel":"self","href":"https://api.thousandeyes.com/v6/tests/1226221"},{"rel":"data","href":"https://api.thousandeyes.com/v6/web/dns-dnssec/1226221"},{"rel":"data","href":"https://api.thousandeyes.com/v6/net/metrics/1226221"},{"rel":"data","href":"https://api.thousandeyes.com/v6/net/path-vis/1226221"},{"rel":"data","href":"https://api.thousandeyes.com/v6/net/bgp-metrics/1226221"}]}]}`
+	setup()
+	var client = &Client{APIEndpoint: server.URL, AuthToken: "foo"}
+	mux.HandleFunc("/tests/dns-dnssec/new.json", func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, "POST", r.Method)
+		w.WriteHeader(http.StatusCreated)
+		_, _ = w.Write([]byte(out))
+	})
+
+	expected := DNSSec{
+		TestID:        122621,
+		Enabled:       1,
+		CreatedBy:     "William Fleming (wfleming@grumpysysadm.com)",
+		CreatedDate:   "2020-02-06 15:28:07",
+		SavedEvent:    0,
+		TestName:      "test123",
+		Type:          "dns-dnssec",
+		Interval:      300,
+		AlertsEnabled: 1,
+		Domain:        "webex.com",
+		Agents: []Agent{
+			{
+				AgentID:     48620,
+				AgentType:   "Cloud",
+				AgentName:   "Seattle, WA (Trial) - IPv6",
+				CountryID:   "US",
+				IPAddresses: []string{"135.84.184.153"},
+				Location:    "Seattle Area",
+				Network:     "Astute Hosting Inc. (AS 54527)",
+				Prefix:      "135.84.184.0/22",
+			},
+		},
+		SharedWithAccounts: []AccountGroup{
+			{
+				Aid:  176592,
+				Name: "Cloudreach",
+			},
+		},
+
+		APILinks: []APILink{
+			{
+				Href: "https://api.thousandeyes.com/v6/tests/1226221",
+				Rel:  "self",
+			},
+			{
+				Href: "https://api.thousandeyes.com/v6/web/dns-dnssec/1226221",
+				Rel:  "data",
+			},
+			{
+				Href: "https://api.thousandeyes.com/v6/net/metrics/1226221",
+				Rel:  "data",
+			},
+			{
+				Href: "https://api.thousandeyes.com/v6/net/path-vis/1226221",
+				Rel:  "data",
+			},
+			{
+				Href: "https://api.thousandeyes.com/v6/net/bgp-metrics/1226221",
+				Rel:  "data",
+			},
+		},
+	}
+	create := DNSSec{
+		TestName: "test123",
+		Domain:   "webex.com",
+		Interval: 300,
+	}
+	res, err := client.CreateDNSSec(create)
+	teardown()
+	assert.Nil(t, err)
+	assert.Equal(t, &expected, res)
+}
+
+func TestClient_DeleteDNSSec(t *testing.T) {
+	setup()
+	mux.HandleFunc("/tests/dns-dnssec/1/delete.json", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusNoContent)
+		assert.Equal(t, "POST", r.Method)
+	})
+
+	var client = &Client{APIEndpoint: server.URL, AuthToken: "foo"}
+	id := 1
+	err := client.DeleteDNSSec(id)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestClient_AddDNSSecAlertRule(t *testing.T) {
+	test := DNSSec{TestName: "test", AlertRules: []AlertRule{}}
+	expected := DNSSec{TestName: "test", AlertRules: []AlertRule{{RuleID: 1}}}
+	test.AddAlertRule(1)
+	assert.Equal(t, expected, test)
+}
+
+func TestClient_UpdateDNSSec(t *testing.T) {
+	setup()
+	out := `{"test":[{"testId":1,"testName":"test123","type":"dns-dnssec","domain":"webex.com" }]}`
+	mux.HandleFunc("/tests/dns-dnssec/1/update.json", func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, "POST", r.Method)
+		_, _ = w.Write([]byte(out))
+	})
+
+	var client = &Client{APIEndpoint: server.URL, AuthToken: "foo"}
+	id := 1
+	dnsp := DNSSec{Domain: "webex.com"}
+	res, err := client.UpdateDNSSec(id, dnsp)
+	if err != nil {
+		t.Fatal(err)
+	}
+	expected := DNSSec{TestID: 1, TestName: "test123", Type: "dns-dnssec", Domain: "webex.com"}
+	assert.Equal(t, &expected, res)
+
+}
+
+func TestDNSSec_AddAgent(t *testing.T) {
+	test := DNSSec{TestName: "test", Agents: Agents{}}
+	expected := DNSSec{TestName: "test", Agents: []Agent{{AgentID: 1}}}
+	test.AddAgent(1)
+	assert.Equal(t, expected, test)
+}
+
+func TestClient_GetDNSSecError(t *testing.T) {
+	setup()
+	var client = &Client{APIEndpoint: server.URL, AuthToken: "foo"}
+	mux.HandleFunc("/tests/dns-dnssec/1.json", func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, "GET", r.Method)
+		w.WriteHeader(http.StatusBadRequest)
+	})
+
+	_, err := client.GetDNSSec(1)
+	teardown()
+	assert.Error(t, err)
+}
+
+func TestClient_GetDNSSecStatusCode(t *testing.T) {
+	setup()
+	out := `{"test":[{"testId":1,"testName":"test123","type":"dnsp-trace"}]}`
+	var client = &Client{APIEndpoint: server.URL, AuthToken: "foo"}
+	mux.HandleFunc("/tests/1.json", func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, "GET", r.Method)
+		w.WriteHeader(http.StatusBadRequest)
+		_, _ = w.Write([]byte(out))
+	})
+
+	_, err := client.GetDNSSec(1)
+	teardown()
+	assert.EqualError(t, err, "Failed call API endpoint. HTTP response code: 400. Error: &{}")
+}
+
+func TestClient_CreateDNSSecStatusCode(t *testing.T) {
+	setup()
+	var client = &Client{APIEndpoint: server.URL, AuthToken: "foo"}
+	mux.HandleFunc("/tests/dns-dnssec/new.json", func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, "POST", r.Method)
+		w.WriteHeader(http.StatusBadRequest)
+		_, _ = w.Write([]byte(`{}`))
+	})
+	_, err := client.CreateDNSSec(DNSSec{})
+	teardown()
+	assert.EqualError(t, err, "Failed call API endpoint. HTTP response code: 400. Error: &{}")
+}
+
+func TestClient_UpdateDNSSecStatusCode(t *testing.T) {
+	setup()
+	var client = &Client{APIEndpoint: server.URL, AuthToken: "foo"}
+	mux.HandleFunc("/tests/dns-dnssec/1/update.json", func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, "POST", r.Method)
+		w.WriteHeader(http.StatusBadRequest)
+		_, _ = w.Write([]byte(`{}`))
+	})
+	_, err := client.UpdateDNSSec(1, DNSSec{})
+	teardown()
+	assert.EqualError(t, err, "Failed call API endpoint. HTTP response code: 400. Error: &{}")
+}
+
+func TestClient_DeleteDNSSecStatusCode(t *testing.T) {
+	setup()
+	var client = &Client{APIEndpoint: server.URL, AuthToken: "foo"}
+	mux.HandleFunc("/tests/dns-dnssec/1/delete.json", func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, "POST", r.Method)
+		w.WriteHeader(http.StatusBadRequest)
+		_, _ = w.Write([]byte(`{}`))
+	})
+	err := client.DeleteDNSSec(1)
+	teardown()
+	assert.EqualError(t, err, "Failed call API endpoint. HTTP response code: 400. Error: &{}")
+}
