8b198a6adebc8c10972fd9580a4ffeeb4ffb6948
William Fleming <wfleming@grumpysysadm.com>
2020-04-11 11:00:01 +0100

    feat(users): linting

--- role.go
+++ role.go
@@ -3,7 +3,7 @@ package thousandeyes
 // AccountGroupRole - an account group role
 type AccountGroupRole struct {
 	RoleName                 string       `json:"roleName,omitempty"`
-	RoleId                   int          `json:"roleId,omitempty"`
+	RoleID                   int          `json:"roleId,omitempty"`
 	HasManagementPermissions int          `json:"hasManagementPermissions,omitempty"`
 	Builtin                  int          `json:"builtin,omitempty"`
 	Permissions              []Permission `json:"permissions,omitempty"`
@@ -13,5 +13,5 @@ type AccountGroupRole struct {
 type Permission struct {
 	IsManagementPermission int    `json:"isManagementPermission"`
 	Label                  string `json:"label"`
-	PermissionId           int    `json:"permissionId"`
+	PermissionID           int    `json:"permissionId"`
 }
