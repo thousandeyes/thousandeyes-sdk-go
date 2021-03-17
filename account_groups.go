46babedbb1b15d6115b090fa475046acc4d06fb1
Joshua Blanchard <joshua.blanchard@gmail.com>
2021-03-17 05:24:58 +0000

    feat(accounts): Support retrieving account groups
    Add support for retrieving account groups list, specifically to support the
    SharedWithAccounts field on tests.

--- account_groups.go
+++ account_groups.go
@@ -1,5 +1,7 @@
 package thousandeyes
 
+import "fmt"
+
 // AccountGroups - list of account groups
 type AccountGroups []AccountGroup
 
@@ -15,4 +17,33 @@ type AccountGroup struct {
 type SharedWithAccount struct {
 	AccountGroupName string `json:"name,omitempty"`
 	AID              int    `json:"aid,omitempty"`
+}
+
+// GetIntegrations - Get third party and webhook integrations
+func (c *Client) GetAccountGroups() (*[]SharedWithAccount, error) {
+	resp, err := c.get("/account-groups")
+	if err != nil {
+		return nil, err
+	}
+	var target map[string][]AccountGroup
+
+	if dErr := c.decodeJSON(resp, &target); dErr != nil {
+		return nil, fmt.Errorf("Could not decode JSON response: %v", dErr)
+	}
+	if _, ok := target["accountGroups"]; !ok {
+		return nil, fmt.Errorf("'accountGroups' not found in JSON response")
+	}
+	fmt.Printf("%v", target)
+
+	// Since the use of this is for the SharedWithAccount configuration,
+	// we will return that type.
+	var accountGroups []SharedWithAccount
+	for _, v := range target["accountGroups"] {
+		account := SharedWithAccount{
+			AccountGroupName: v.AccountGroupName,
+			AID:              v.AID,
+		}
+		accountGroups = append(accountGroups, account)
+	}
+	return &accountGroups, nil
 }
