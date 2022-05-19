eb0f812e5b9bce1a2a7d79b6ba8d4eae145097b3
Raul Benencia <raul@thousandeyes.com>
2022-05-19 11:24:19 -0700

    refactor(monitor)!: use pointers in struct fields

--- monitors.go
+++ monitors.go
@@ -5,10 +5,10 @@ type Monitors []Monitor
 
 // Monitor - A monitor
 type Monitor struct {
-	MonitorID   int    `json:"monitorId,omitempty"`
-	IPAddress   string `json:"ipAddress,omitempty"`
-	CountryID   string `json:"countryId,omitempty"`
-	MonitorName string `json:"monitorName,omitempty"`
-	Network     string `json:"network,omitempty"`
-	MonitorType string `json:"monitorType,omitempty"`
+	MonitorID   *int    `json:"monitorId,omitempty"`
+	IPAddress   *string `json:"ipAddress,omitempty"`
+	CountryID   *string `json:"countryId,omitempty"`
+	MonitorName *string `json:"monitorName,omitempty"`
+	Network     *string `json:"network,omitempty"`
+	MonitorType *string `json:"monitorType,omitempty"`
 }
