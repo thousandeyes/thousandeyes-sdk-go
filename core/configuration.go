2f5ad76b612609899412db86a9961fb330d50fa6
Phellippe Lima <phpinhei@thousandeyes.com>
2025-02-17 14:22:37 +0000

    feat: small comment changes

--- core/configuration.go
+++ core/configuration.go
@@ -7,31 +7,22 @@ import (
 
 // Configuration stores the configuration of the API client
 type Configuration struct {
-	Host          string            `json:"host,omitempty"`
-	Scheme        string            `json:"scheme,omitempty"`
-	DefaultHeader map[string]string `json:"defaultHeader,omitempty"`
-	UserAgent     string            `json:"userAgent,omitempty"`
-	Debug         bool              `json:"debug,omitempty"`
-	ServerURL     string
-	AuthToken     string
-	HTTPClient    *http.Client
-	Context       context.Context
+	UserAgent  string `json:"userAgent,omitempty"`
+	Debug      bool   `json:"debug,omitempty"`
+	ServerURL  string
+	AuthToken  string
+	HTTPClient *http.Client
+	Context    context.Context
 }
 
 // NewConfiguration returns a new Configuration object
 func NewConfiguration() *Configuration {
 	cfg := &Configuration{
-		DefaultHeader: make(map[string]string),
-		UserAgent:     "ThousandEyes Go SDK v3",
-		Debug:         false,
-		ServerURL:     "https://api.thousandeyes.com/v7",
+		UserAgent: "ThousandEyes Go SDK v3",
+		Debug:     false,
+		ServerURL: "https://api.thousandeyes.com/v7",
 	}
 	return cfg
-}
-
-// AddDefaultHeader adds a new HTTP header to the default header in the request
-func (c *Configuration) AddDefaultHeader(key string, value string) {
-	c.DefaultHeader[key] = value
 }
 
 func (c *Configuration) WithAuthToken(authToken string) *Configuration {
