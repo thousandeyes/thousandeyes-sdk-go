8f1f4d97bb8c914624b258b57bdbe056bb370783
API Team <api-team@thousandeyes.com>
2026-02-03 09:52:19 +0000

    [GitHub Bot] Generated go SDK

--- tests/api_ftp_server_tests.go
+++ tests/api_ftp_server_tests.go
@@ -633,7 +633,7 @@ func (r ApiUpdateFtpServerTestRequest) Execute() (*FtpServerTestResponse, *http.
 /*
 UpdateFtpServerTest Update FTP Server test
 
-Updates a FTP Server test. The target test cannot be a live share or saved event. This method requires Account Admin permissions.
+Updates a FTP Server test. Shared tests have limited updating capabilities. Only account-specific configurations may be updated, namely: Alert rules, Alert suppression windows, Labels. This method requires Account Admin permissions.
 **Note**: **Saved Events** are now called **Private Snapshots** in the user interface. This change does not affect API.
 
  @param testId Test ID
